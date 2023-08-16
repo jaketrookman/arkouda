5cca1eb94a124d89e359783b74c18ffc23da43f8
Pierce Hayes <pierce314159@users.noreply.github.com>
2023-08-16 18:42:46 -0400

    a few changes to series test

--- PROTO_tests/tests/series_test.py
+++ PROTO_tests/tests/series_test.py
@@ -1,214 +1,147 @@
 import pandas as pd
 import pytest
+from pandas.testing import assert_frame_equal
 
 import arkouda as ak
 
 
 class TestSeries:
     def test_series_creation(self):
-        # Use positional arguments
-        ar_tuple = ak.arange(3), ak.arange(3)
-        s = ak.Series(ar_tuple)
-        assert isinstance(s, ak.Series)
+        idx = ak.arange(3)
+        for val in ak.arange(3), ak.array(["A", "B", "C"]):
+            ans = ak.Series(data=val, index=idx).to_list()
+            for series in (
+                ak.Series(data=val, index=idx),
+                ak.Series(data=val),
+                ak.Series(val, idx),
+                ak.Series(val),
+                ak.Series((idx, val)),
+            ):
+                assert isinstance(series, ak.Series)
+                assert isinstance(series.index, ak.Index)
+                assert series.to_list() == ans
 
-        ar_tuple = ak.array(["A", "B", "C"]), ak.arange(3)
-        s = ak.Series(ar_tuple)
-        assert isinstance(s, ak.Series)
-
-        # Both data and index are supplied
-        v = ak.array(["A", "B", "C"])
-        i = ak.arange(3)
-        s = ak.Series(data=v, index=i)
-
-        assert isinstance(s, ak.Series)
-        assert isinstance(s.index, ak.Index)
-
-        # Just data is supplied
-        s = ak.Series(data=v)
-        assert isinstance(s, ak.Series)
-        assert isinstance(s.index, ak.Index)
-
-        # Just index is supplied (keyword argument)
         with pytest.raises(TypeError):
-            s = ak.Series(index=i)
+            ak.Series(index=idx)
 
-        # Just data is supplied (positional argument)
-        s = ak.Series(ak.array(["A", "B", "C"]))
-        assert isinstance(s, ak.Series)
-
-        # Just index is supplied (ar_tuple argument)
-        ar_tuple = (ak.arange(3),)
         with pytest.raises(TypeError):
-            s = ak.Series(ar_tuple)
+            ak.Series((ak.arange(3),))
 
-        # No arguments are supplied
         with pytest.raises(TypeError):
-            s = ak.Series()
+            ak.Series()
 
         with pytest.raises(ValueError):
-            s = ak.Series(data=ak.arange(3), index=ak.arange(6))
+            ak.Series(data=ak.arange(3), index=ak.arange(6))
 
     def test_lookup(self):
-        v = ak.array(["A", "B", "C"])
-        i = ak.arange(3)
-        s = ak.Series(data=v, index=i)
+        for val in ak.arange(3), ak.array(["A", "B", "C"]):
+            s = ak.Series(data=val, index=ak.arange(3))
 
-        lk = s.locate(1)
-        assert isinstance(lk, ak.Series)
-        assert lk.index[0] == 1
-        assert lk.values[0] == "B"
+            for key in 1, [0, 2], ak.Index([1]), ak.Index([0, 2]):
+                lk = s.locate(key)
+                assert isinstance(lk, ak.Series)
+                key = ak.array(key) if not isinstance(key, int) else key
+                assert (lk.index == s.index[key]).all()
+                assert (lk.values == s.values[key]).all()
 
-        lk = s.locate([0, 2])
-        assert isinstance(lk, ak.Series)
-        assert lk.index[0] == 0
-        assert lk.values[0] == "A"
-        assert lk.index[1] == 2
-        assert lk.values[1] == "C"
+            # testing multi-index lookup
+            mi = ak.MultiIndex([ak.arange(3), ak.array([2, 1, 0])])
+            s = ak.Series(data=val, index=mi)
+            lk = s.locate(mi[0])
+            assert isinstance(lk, ak.Series)
+            assert lk.index.index == mi[0].index
+            assert lk.values[0] == val[0]
 
-        # testing index lookup
-        i = ak.Index([1])
-        lk = s.locate(i)
-        assert isinstance(lk, ak.Series)
-        assert lk.index.to_list() == i.index.to_list()
-        assert lk.values[0] == v[1]
+            # ensure error with scalar and multi-index
+            with pytest.raises(TypeError):
+                s.locate(0)
 
-        i = ak.Index([0, 2])
-        lk = s.locate(i)
-        assert isinstance(lk, ak.Series)
-        assert lk.index.to_list() == i.index.to_list()
-        assert lk.values.to_list() == v[ak.array([0, 2])].to_list()
-
-        # testing multi-index lookup
-        mi = ak.MultiIndex([ak.arange(3), ak.array([2, 1, 0])])
-        s = ak.Series(data=v, index=mi)
-        lk = s.locate(mi[0])
-        assert isinstance(lk, ak.Series)
-        assert lk.index.index == mi[0].index
-        assert lk.values[0] == v[0]
-
-        # ensure error with scalar and multi-index
-        with pytest.raises(TypeError):
-            lk = s.locate(0)
-
-        with pytest.raises(TypeError):
-            lk = s.locate([0, 2])
-
-    def test_shape(self):
-        v = ak.array(["A", "B", "C"])
-        i = ak.arange(3)
-        s = ak.Series(data=v, index=i)
-
-        (dim,) = s.shape
-        assert dim == 3
+            with pytest.raises(TypeError):
+                s.locate([0, 2])
 
     def test_add(self):
-        i = ak.arange(3)
-        v = ak.arange(3, 6, 1)
-        s = ak.Series(data=i, index=i)
-
-        s_add = ak.Series(data=v, index=v)
-
-        added = s.add(s_add)
-
-        idx_list = added.index.to_pandas().tolist()
-        val_list = added.values.to_list()
-        for i in range(6):
-            assert i in idx_list and val_list
+        added = ak.Series(ak.arange(3)).add(ak.Series(data=ak.arange(3, 6), index=ak.arange(3, 6)))
+        assert added.index.to_list() == list(range(6))
+        assert added.values.to_list() == list(range(6))
 
     def test_topn(self):
-        v = ak.arange(3)
-        s = ak.Series(data=v, index=v)
+        top = ak.Series(ak.arange(30)).topn(10)
+        assert top.values.to_list() == list(range(29, 19, -1))
+        assert top.index.to_list() == list(range(29, 19, -1))
 
-        top = s.topn(2)
-        assert top.index.to_pandas().tolist() == [2, 1]
-        assert top.values.to_list() == [2, 1]
+    def test_sort(self):
+        ordered = ak.arange(5)
+        perm = ak.array([3, 1, 4, 0, 2])
 
-    def test_sort_idx(self):
-        v = ak.arange(5)
-        i = ak.array([3, 1, 4, 0, 2])
-        s = ak.Series(data=v, index=i)
+        idx_sort = ak.Series(data=ordered, index=perm).sort_index()
+        assert idx_sort.index.to_list() == ordered.to_list()
+        assert (idx_sort.values == perm).all()
 
-        sorted = s.sort_index()
-        assert sorted.index.to_pandas().tolist() == [i for i in range(5)]
-        assert sorted.values.to_list() == [3, 1, 4, 0, 2]
-
-    def test_sort_value(self):
-        v = ak.array([3, 1, 4, 0, 2])
-        i = ak.arange(5)
-        s = ak.Series(data=v, index=i)
-
-        sorted = s.sort_values()
-        assert sorted.values.to_list() == [i for i in range(5)]
-        assert sorted.index.to_pandas().tolist() == [3, 1, 4, 0, 2]
+        val_sort = ak.Series(data=perm, index=ordered).sort_values()
+        assert val_sort.index.to_pandas().tolist() == perm.to_list()
+        assert (val_sort.values == ordered).all()
 
     def test_head_tail(self):
-        v = ak.arange(5)
-        s = ak.Series(data=v, index=v)
+        n = 10
+        s = ak.Series(ak.arange(n))
 
-        head = s.head(2)
-        assert head.index.to_pandas().tolist() == [0, 1]
-        assert head.values.to_list() == [0, 1]
+        for i in range(n):
+            head = s.head(i)
+            assert head.index.to_list() == list(range(i))
+            assert head.values.to_list() == list(range(i))
 
-        tail = s.tail(3)
-        assert tail.index.to_pandas().tolist() == [2, 3, 4]
-        assert tail.values.to_list() == [2, 3, 4]
+            tail = s.tail(i)
+            assert tail.index.to_list() == ak.arange(n)[-i:n].to_list()
+            assert tail.values.to_list() == ak.arange(n)[-i:n].to_list()
 
     def test_value_counts(self):
-        v = ak.array([0, 0, 1, 2, 2])
-        i = ak.arange(5)
-        s = ak.Series(data=v, index=i)
+        s = ak.Series(ak.array([1, 2, 0, 2, 0]))
 
         c = s.value_counts()
-        assert c.index.to_pandas().tolist() == [0, 2, 1]
+        assert c.index.to_list() == [0, 2, 1]
         assert c.values.to_list() == [2, 2, 1]
 
-        c = s.value_counts(sort=True)
-        assert c.index.to_pandas().tolist() == [0, 2, 1]
-        assert c.values.to_list() == [2, 2, 1]
+        c = s.value_counts(sort=False)
+        assert c.index.to_list() == list(range(3))
+        assert c.values.to_list() == [2, 1, 2]
 
     def test_concat(self):
-        v = ak.arange(5)
-        s = ak.Series(data=v, index=v)
-
-        v = ak.arange(5, 11, 1)
-        s2 = ak.Series(data=v, index=v)
-
-        c = ak.Series.concat([s, s2])
-        assert c.index.to_pandas().tolist() == [i for i in range(11)]
-        assert c.values.to_list() == [i for i in range(11)]
+        s = ak.Series(ak.arange(5))
+        s2 = ak.Series(ak.arange(5, 11), ak.arange(5, 11))
+        s3 = ak.Series(ak.arange(5, 10), ak.arange(5, 10))
 
         df = ak.Series.concat([s, s2], axis=1)
         assert isinstance(df, ak.DataFrame)
 
         ref_df = pd.DataFrame(
             {
-                "idx": [i for i in range(11)],
+                "idx": list(range(11)),
                 "val_0": [0, 1, 2, 3, 4, 0, 0, 0, 0, 0, 0],
                 "val_1": [0, 0, 0, 0, 0, 5, 6, 7, 8, 9, 10],
             }
         )
-        assert ((ref_df == df.to_pandas()).all()).all()
+        assert_frame_equal(ref_df, df.to_pandas())
 
-    def test_pdconcat(self):
-        v = ak.arange(5)
-        s = ak.Series(data=v, index=v)
+        def list_helper(arr):
+            return arr.to_list() if isinstance(arr, (ak.pdarray, ak.Index)) else arr.tolist()
 
-        v = ak.arange(5, 11, 1)
-        s2 = ak.Series(data=v, index=v)
+        for fname in "concat", "pdconcat":
+            func = getattr(ak.Series, fname)
+            c = func([s, s2])
+            assert list_helper(c.index) == list(range(11))
+            assert list_helper(c.values) == list(range(11))
 
-        c = ak.Series.pdconcat([s, s2])
-        assert isinstance(c, pd.Series)
-        assert c.index.tolist() == [i for i in range(11)]
-        assert c.values.tolist() == [i for i in range(11)]
-
-        v = ak.arange(5, 10, 1)
-        s2 = ak.Series(data=v, index=v)
-
-        df = ak.Series.pdconcat([s, s2], axis=1)
-        assert isinstance(df, pd.DataFrame)
-
-        ref_df = pd.DataFrame({0: [0, 1, 2, 3, 4], 1: [5, 6, 7, 8, 9]})
-        assert (ref_df == df).all().all()
+            df = func([s, s3], axis=1)
+            if fname == "concat":
+                ref_df = pd.DataFrame(
+                    {"idx": [0, 1, 2, 3, 4], "val_0": [0, 1, 2, 3, 4], "val_1": [5, 6, 7, 8, 9]}
+                )
+                assert isinstance(df, ak.DataFrame)
+                assert_frame_equal(ref_df, df.to_pandas())
+            else:
+                ref_df = pd.DataFrame({0: [0, 1, 2, 3, 4], 1: [5, 6, 7, 8, 9]})
+                assert isinstance(df, pd.DataFrame)
+                assert_frame_equal(ref_df, df)
 
     def test_index_as_index_compat(self):
         # added to validate functionality for issue #1506
